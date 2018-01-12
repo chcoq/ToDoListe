ecfadf276f730b3d19ba6a1a6f093c49095d6de4
formation <formation@mail.fr>
2018-01-12 12:19:11 +0100

    Création de l'interface du layout main_task

---
+++ app/src/main/java/dl/fr/todoliste/MainTask.java
@@ -0,0 +1,13 @@
+package dl.fr.todoliste;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainTask extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_task);
+    }
+}
