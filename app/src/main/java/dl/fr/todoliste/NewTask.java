c24136fcf9dcd39e62e58c39e14854d851036045
lecocq <ds>
2018-01-13 20:59:45 +0100

    Création de l'interface du layout NewTask

---
+++ app/src/main/java/dl/fr/todoliste/NewTask.java
@@ -0,0 +1,14 @@
+package dl.fr.todoliste;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewTask extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_task);
+
+    }
+}
